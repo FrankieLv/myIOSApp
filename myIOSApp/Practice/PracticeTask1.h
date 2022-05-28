e3b51e43f89f4ffcc60b1c870ec5aca92ab37162
“lyf.email@qq.comgit config --global user.email “lyf.email@qq.com <lvfrankie@lvyifengdeMacBook-Pro.local>
2022-05-28 14:34:14 +0800

    first commit

---
+++ myIOSApp/Practice/PracticeTask1.h
@@ -0,0 +1,13 @@
+//
+//  PracticeTask1.h
+//  myIOSApp
+//
+//  Created by 吕义丰 on 2022/5/27.
+//
+
+#import <Foundation/Foundation.h>
+@interface PracticeTask1 : NSObject
+
+- (void)convertAndSaveData;
+
+@end
---
+++ myIOSApp/PracticeTask1.m
@@ -0,0 +1,8 @@
+//
+//  PracticeTask1.m
+//  myIOSApp
+//
+//  Created by 吕义丰 on 2022/5/27.
+//
+
+#import <Foundation/Foundation.h>
